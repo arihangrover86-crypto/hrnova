fe38fdee3a9b1af9653d500759072ff25292f17c
arihangrover86-crypto <arihangrover86@gmail.com>
2025-09-05 00:20:07 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),   // ✅ correct alias
+      "@": path.resolve(__dirname, "~client/src"),   // ✅ correct alias
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
